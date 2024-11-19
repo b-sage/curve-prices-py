b1b94dd73717aa1acd3d5a55c3d7afb3491d9e32
Ben <benjamin.g.sage@gmail.com>
2024-11-19 16:49:50 -0700

    clients

---
+++ curve_prices/dao_client.py
@@ -0,0 +1,105 @@
+from curve_prices.client_core import ClientCore
+
+class DaoClient(ClientCore):
+
+    path = 'dao/'
+
+    def get_gauges_overview(self):
+        endpoint = f"gauges/overview"
+        return self._execute_request(endpoint)
+
+    def get_votes_overview(self):
+        endpoint = f"votes/overview"
+        return self._execute_request(endpoint)
+
+    def get_user_gauge_votes(self, user: str):
+        endpoint = f"gauges/votes/user/{user}"
+        return self._execute_request(endpoint)
+
+    def get_user_proposal_votes(self, user: str, pagination: int=1000, page: int=1):
+        endpoint = f"proposals/votes/user/{user}" + self._build_query(False, pagination=pagination, page=page)
+        return self._execute_request(endpoint)
+
+    def get_gauge_for_pool(self, chain: str, pool: str):
+        endpoint = f"pools/{chain}/{pool}/gauge"
+        return self._execute_request(endpoint)
+
+    def get_gauge_for_market(self, chain: str, controller: str):
+        endpoint = f"lending/{chain}/{controller}/gauge"
+        return self._execute_request(endpoint)
+
+    def get_votes_for_gauge(self, gauge: str):
+        endpoint = f"gauges/{gauge}/votes"
+        return self._execute_request(endpoint)
+
+    def get_gauge_metadata(self, gauge: str):
+        endpoint = f"gauges/{gauge}/metadata"
+        return self._execute_request(endpoint)
+
+    def get_gauge_weight_history(self, gauge: str):
+        endpoint = f"gauges/{gauge}/weight_history"
+        return self._execute_request(endpoint)
+
+    def get_gauge_deployment(self, gauge: str):
+        endpoint = f"gauges/{gauge}/deployment"
+        return self._execute_request(endpoint)
+
+    def get_user_crv_locks(self, user: str):
+        endpoint = f"locks/{user}"
+        return self._execute_request(endpoint)
+
+    def get_daily_crv_lock_history(self, days_back: int):
+        endpoint = f"locks/daily/{days_back}"
+        return self._execute_request(endpoint)
+
+    def get_crv_supply_breakdown(self, days_back: int):
+        endpoint = f"supply/{days_back}"
+        return self._execute_request(endpoint)
+
+    def get_largest_lockers(self, pagination: int=1000, page: int=1):
+        endpoint = f"lockers" + self._build_query(False, pagination=pagination, page=page)
+        return self._execute_request(endpoint)
+
+    def get_proposals(self, search_string: str=None, pagination: int=1000, page: int=1, status_filter:str='all', type_filter: str='all'):
+        endpoint = f"proposals" + self._build_query(
+            False,
+            search_string=search_string,
+            pagination=pagination,
+            page=page,
+            status_filter=status_filter,
+            type_filter=type_filter
+        )
+        return self._execute_request(endpoint)
+
+    def get_proposal(self, type_: str, vote_id: int):
+        endpoint = f"proposals/details/{type}/{vote_id}"
+        return self._execute_request(endpoint)
+
+    def get_fee_distributions(self, page: int=1, per_page: int=1000):
+        endpoint = f"fees/distributions" + self._build_query(False, page=page, per_page=per_page)
+        return self._execute_request(endpoint)
+
+    def get_weekly_crvusd_fees(self, start: int=None, end: int=None):
+        endpoint = f"fees/crvusd/weekly" + self._build_query(False, start=start, end=end)
+        return self._execute_request(endpoint)
+
+    def get_weekly_pool_fees(self, start: int=None, end: int=None):
+        endpoint = f"fees/pools/weekly" + self._build_query(False, start=start, end=end)
+        return self._execute_request(endpoint)
+
+    def get_pending_pool_fees(self, chain: str):
+        endpoint = f"fees/{chain}/pending"
+        return self._execute_request(endpoint)
+
+    def get_fee_settlements(self, prior_to: int):
+        endpoint = f"fees/settlements" + self._build_query(False, timestamp=prior_to)
+        return self._execute_request(endpoint)
+
+    def get_collected_fees(self):
+        endpoint = f"fees/collected"
+        return self._execute_request(endpoint)
+
+    def get_staged_fees(self):
+        endpoint = f"fees/staged"
+        return self._execute_request(endpoint)
+
---
+++ curve_prices/lending_client.py
@@ -0,0 +1,185 @@
+from curve_prices.client_core import ClientCore
+
+class LendingClient(ClientCore):
+
+    path = 'lending/'
+
+    def get_chains(self):
+        endpoint = f"chains/"
+        return self._execute_request(endpoint)    
+
+    def get_llamma_ohlc(
+        self, 
+        chain: str,
+        address: str,
+        start: int,
+        end: int,
+        agg_number: int=None,
+        agg_units: str=None
+    ):
+        endpoint = f"llamma_ohlc/{chain}/{address}" + self._build_query(
+                False,
+                start=start,
+                end=end,
+                agg_number=agg_number,
+                agg_units=agg_units
+        )
+        return self._execute_request(endpoint)
+
+    def get_oracle_ohlc(
+        self,
+        chain: str,
+        controller: str,
+        start: int,
+        end: int,
+        agg_number: int=None,
+        agg_units: str=None
+    ):
+        endpoint = f"oracle_ohlc/{chain}/{controller}" + self._build_query(
+                False,
+                start=start,
+                end=end,
+                agg_number=agg_number,
+                agg_units=agg_units
+        )
+        return self._execute_request(endpoint) 
+
+    def get_market(self, chain: str, controller: str, fetch_on_chain: bool=False, agg: str='day'):
+        endpoint = f"markets/{chain}/{controller}/snapshots" + self._build_query(
+            False,
+            fetch_on_chain=fetch_on_chain,
+            agg=agg
+        )
+        return self._execute_request(endpoint)
+
+    def get_market_loan_distribution(self, chain: str, controller: str):
+        endpoint = f"markets/{chain}/{controller}/loans/distribution"
+        return self._execute_request(endpoint)
+
+    def get_market_llamma_events(self, chain: str, address: str, page: int=1, per_page: int=1000):
+        endpoint = f"llamma_events/{chain}/{address}" + self._build_query(False, page=page, per_page=per_page)
+        return self._execute_request(endpoint)
+
+    def get_market_llamma_trades(self, chain: str, address: str, page: int=1, per_page: int=1000):
+        endpoint = f"llamma_trades/{chain}/{address}" + self._build_query(False, page=page, per_page=per_page)
+        return self._execute_request(endpoint)
+
+    def get_market_collateral_events(
+        self, 
+        chain: str, 
+        controller: str, 
+        user: str,
+        pagination: int=1000,
+        page: int=1
+    ):
+        endpoint = f"collateral_events/{chain}/{controller}/{user}" + self._build_query(
+            False, 
+            pagination=pagination, 
+            page=page
+        )
+        return self._execute_request(endpoint)
+
+    def get_market_soft_liquidation_ratio(self, chain: str, controller: str, start: int=None, end: int=None):
+        endpoint = f"liquidations/{chain}/{controller}/soft_liquidation_ratio" + self._build_query(
+            False,
+            start=start,
+            end=end
+        )
+        return self._execute_request(endpoint)
+
+    def get_market_detailed_liquidation_history(self, chain: str, controller: str, start: int=None, end: int=None):
+        endpoint = f"liquidations/{chain}/{controller}/history/detailed" + self._build_query(
+            False,
+            start=start,
+            end=end
+        )
+        return self._execute_request(endpoint)
+
+    def get_market_aggregated_liquidation_history(self, chain: str, controller: str, start: int=None, end: int=None):
+        endpoint = f"liquidations/{chain}/{controller}/history/aggregated" + self._build_query(
+            False, 
+            start=start, 
+            end=end
+        )
+        return self._execute_request(endpoint)
+
+    def get_aggregated_liquidation_history(self, chain: str, start: int=None, end: int=None):
+        endpoint = f"liquidations/{chain}/history/aggregated" + self._build_query(
+            False,
+            start=start,
+            end=end
+        )
+        return self._execute_request(endpoint)
+
+    def get_market_losses_history(self, chain: str, controller: str, start: int=None, end: int=None):
+        endpoint = f"liquidations/{chain}/{controller}/losses/history" + self._build_query(False, start=start, end=end)
+        return self._execute_request(endpoint)
+
+    def get_market_health_distribution(self, chain: str, controller: str):
+        endpoint = f"liquidations/{chain}/{controller}/health/distribution"
+        return self._execute_request(endpoint)
+
+    def get_market_cr_distirubtion(self, chain: str, controller: str):
+        endpoint = f"liquidations/{chain}/{controller}/cr/distribution"
+        return self._execute_request(endpoint)
+
+    def get_market_health_overview(self, chain: str, controller: str, fetch_on_chain: bool=False):
+        endpoint = f"liquidations/{chain}/{controller}/overview" + self._build_query(False, fetch_on_chain=fetch_on_chain)
+        return self._execute_request(endpoint)
+
+    def get_losses_per_band_range(self, chain: str, controller: str, start: int=None, end: int=None, statistic: str='mean'):
+        endpoint = f"liquidations/{chain}/{controller}/losses_per_band_range" + self._build_query(
+            False,
+            start=start,
+            end=end,
+            statistic=statistic
+        )
+        return self._execute_request(endpoint)
+
+    def get_hard_liquidated_users(self, chain: str, controller: str, start: int=None, end: int=None):
+        endpoint = f"liquidations/{chain}/{controller}/hard/users" + self._build_query(
+            False,
+            start=start,
+            end=end
+        )
+        return self._execute_request(endpoint)
+
+    def get_hard_liquidation_total(self, chain: str, controller: str):
+        endpoint = f"liquidations/{chain}/{controller}/hard/total"
+        return self._execute_request(endpoint)
+
+    def get_user_markets(self, chain: str, user: str, page: int=1, per_page: int=1000):
+        endpoint = f"users/{chain}/{user}" + self._build_query(False, page=page, per_page=per_page)
+        return self._execute_request(endpoint)
+
+    def get_user_market_stats(self, chain: str, user: str, controller: str):
+        endpoint = f"users/{chain}/{user}/{controller}/stats"
+        return self._execute_request(endpoint)
+
+    def get_user_market_snapshot(
+        self,
+        chain: str,
+        user: str,
+        contoller: str,
+        page: int=1,
+        per_page: int=1000,
+        start: int=None,
+        end: int=None
+    ):
+        endpoint = f"users/{chain}/{user}/{contoller}/snapshots" + self._build_query(
+            False,
+            page=page,
+            per_page=per_page,
+            start=start,
+            end=end
+        )
+        return self._execute_request(endpoint)
+
+    def get_market_users(self, chain: str, controller: str, page: int=1, per_page: int=1000):
+        endpoint = f"users/{chain}/{contoller}/users" + self._build_query(
+            False,
+            page=page,
+            per_page=per_page
+        )
+        return self._execute_request(endpoint)
+
