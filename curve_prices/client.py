dbf0e69c0664d8fd0d1281c6f7dce18a68e21b49
Ben <benjamin.g.sage@gmail.com>
2024-11-19 13:53:46 -0700

    updates

--- curve_prices/client.py
+++ curve_prices/client.py
@@ -24,8 +24,8 @@ class CurvePricesClient:
         endpoint = 'chains/'
         return self._execute_request(endpoint)
 
-    def chain(self, chain_name: str, per_page: int=1000, page: int=1):
-        endpoint = f"chains/{chain_name}" + self._build_query(False, per_page=per_page, page=page)
+    def chain(self, chain: str, per_page: int=1000, page: int=1):
+        endpoint = f"chains/{chain}" + self._build_query(False, per_page=per_page, page=page)
         return self._execute_request(endpoint)
 
     def transaction_activity(self, start: int=None, end: int=None):
@@ -40,18 +40,18 @@ class CurvePricesClient:
         endpoint = f"lending/chains/"
         return self._execute_request(endpoint)
 
-    def pool_metadata(self, chain: str, pool_address: str):
-        endpoint = f"pools/{chain}/{pool_address}/metadata"
+    def pool_metadata(self, chain: str, address: str):
+        endpoint = f"pools/{chain}/{address}/metadata"
         return self._execute_request(endpoint)
 
-    def pool_data(self, chain: str, pool_address: str):
-        endpoint = f"pools/{chain}/{pool_address}"
+    def pool_data(self, chain: str, address: str):
+        endpoint = f"pools/{chain}/{address}"
         return self._execute_request(endpoint)
 
     def ohlc(
         self, 
         chain: str, 
-        pool_address: str, 
+        address: str, 
         main_token: str, 
         reference_token: str, 
         start: int, 
@@ -59,7 +59,7 @@ class CurvePricesClient:
         agg_number: int=None, 
         agg_units: str=None
     ):
-        endpoint = f"ohlc/{chain}/{pool_address}" + self._build_query(
+        endpoint = f"ohlc/{chain}/{address}" + self._build_query(
                 False, 
                 main_token=main_token, 
                 reference_token=reference_token, 
@@ -73,14 +73,14 @@ class CurvePricesClient:
     def lp_ohlc(
         self,
         chain: str,
-        pool_address: str,
+        address: str,
         start: int,
         end: int,
         agg_number: int=None,
         agg_units: str=None,
         price_units: str='usd'
     ):
-        endpoint = f"lp_ohlc/{chain}/{pool_address}" + self._build_query(
+        endpoint = f"lp_ohlc/{chain}/{address}" + self._build_query(
                 False,
                 start=start,
                 end=end,
@@ -90,8 +90,16 @@ class CurvePricesClient:
         )
         return self._execute_request(endpoint)
 
-    def llamma_ohlc_crvusd(self, chain: str, pool_address: str, start: int, end: int, agg_number: int=None, agg_units: int=None):
-        endpoint = f"crvusd/llamma_ohlc/{chain}/{pool_address}" + self._build_query(
+    def llamma_ohlc_crvusd(
+        self, 
+        chain: str, 
+        address: str, 
+        start: int, 
+        end: int, 
+        agg_number: int=None, 
+        agg_units: int=None
+    ):
+        endpoint = f"crvusd/llamma_ohlc/{chain}/{address}" + self._build_query(
                 False,
                 start=start,
                 end=end,
@@ -99,5 +107,197 @@ class CurvePricesClient:
                 agg_units=agg_units
         )
         return self._execute_request(endpoint)
+
+    def llamma_ohlc_lending(
+        self, 
+        chain: str,
+        address: str,
+        start: int,
+        end: int,
+        agg_number: int=None,
+        agg_units: str=None
+    ):
+        endpoint = f"lending/llamma_ohlc/{chain}/{address}" + self._build_query(
+                False,
+                start=start,
+                end=end,
+                agg_number=agg_number,
+                agg_units=agg_units
+        )
+        return self._execute_request(endpoint)
+
+    def oracle_ohlc(
+        self,
+        chain: str,
+        controller_address: str,
+        start: int,
+        end: int,
+        agg_number: int=None,
+        agg_units: str=None
+    ):
+        endpoint = f"lending/oracle_ohlc/{chain}/{controller_address}" + self._build_query(
+                False,
+                start=start,
+                end=end,
+                agg_number=agg_number,
+                agg_units=agg_units
+        )
+        return self._execute_request(endpoint)
+
+    def trades(
+        self, 
+        chain: str, 
+        address: str, 
+        main_token: str,
+        reference_token: str,
+        page: int=1,
+        per_page: int=1000,
+        include_state: bool=False
+    ):
+        endpoint = f"trades/{chain}/{address}" + self._build_query(
+            False,
+            main_token=main_token,
+            reference_token=reference_token,
+            page=page,
+            per_page=par_page,
+            include_state=include_state
+        )
+        return self._execute_request(endpoint)
+
+    def llamma_trades(self, chain: str, address: str, page: int=1, per_page: int=1000):
+        endpoint = f"crvusd/llamma_trades/{chain}/{address}" + self._build_query(False, page=page, per_page=per_page)
+        return self._execute_request(endpoint)
+
+    def usd_prices(self, chain: str):
+        endpoint = f"usd_price/{chain}"
+        return self._execute_request(endpoint)
+
+    def usd_price(self, chain: str, address: str):
+        endpoint = f"usd_price/{chain}/{address}"
+        return self._execute_request(endpoint)
+
+    def usd_price_history(self, chain: str, address: str, start: int, end: int, interval: str='day'):
+        endpoint = f"usd_price/{chain}/{address}/history" + self._build_query(
+            False,
+            start=start,
+            end=end,
+            interval=interval
+        )
+        return self._execute_request(endpoint)
+
+    def liquidity(self, chain: str, address: str, page: int=1, per_page: int=1000, include_state: bool=False):
+        endpoint = f"liquidity/{chain}/{address}" + self._build_query(
+            False,
+            page=page,
+            per_page=per_page,
+            include_state=include_state
+        )
+        return self._execute_request(endpoint)
+
+    def llammma_liquidity(self, chain: str, address: str, page: int=1, per_page: int=1000):
+        endpoint = f"crvusd/llamma_events/{chain}/{address}" + self._build_query(
+            False,
+            page=page,
+            per_page=per_page
+        )
+        return self._execute_request(endpoint)
+
+    def pool_usd_volume(self, chain: str, address: str, start: int, end: int, interval: str='day'):
+        endpoint = f"volume/usd/{chain}/{address}" + self._build_query(
+            False,
+            start=start,
+            end=end,
+            interval=interval
+        )
+        return self._execute_request(endpoint)
+
+    def pool_token_volume(
+        self, 
+        chain: str,
+        address: str,
+        main_token: str,
+        reference_token: str,
+        start: int,
+        end: int,
+        interval: str="day"
+    ):
+        endpoint = f"volume/{chain}/{address}" + self._build_query(
+            False,
+            main_token=main_token,
+            reference_token=reference_token,
+            start=start,
+            end=end,
+            interval=interval
+        )
+        return self._execute_request(endpoint)
+
+    def chain_usd_volume(self, chain: str, start: int, end: int, interval: str='day'):
+        endpoint = f"volume/{chain}/all" + self._build_query(
+            False,
+            start=start,
+            end=end,
+            interval=interval
+        )
+        return self._execute_request(endpoint)
+
+    def pool_snapshot(self, chain: str, address: str, start: int, end: int):
+        endpoint = f"snapshots/{chain}/{address}" + self._build_query(False, start=start, end=end)
+        return self._execute_request(endpoint)
+
+    def pool_tvl_snapshot(self, chain: str, address: str, start: int, end: int, unit: str='day'):
+        endpoint = f"snapshots/{chain}/{address}/tvl" + self._build_query(False, start=start, end=end, unit=unit)
+        return self._execute_request(endpoint)
+
+    def crvusd_markets(self, chain: str, fetch_on_chain: bool=False, page: int=1, per_page: int=1000):
+        endpoint = f"crvusd/markets/{chain}" + self._build_query(
+            False,
+            fetch_on_chain=fetch_on_chain,
+            page=page,
+            per_page=per_page
+        )
+        return self._execute_request(endpoint)
+
+    def crvusd_market_snapshot(self, chain: str, address: str, fetch_on_chain: bool=False, agg: str='day'):
+        endpoint = f"crvusd/markets/{chain}/{address}" + self._build_query(
+            False,
+            fetch_on_chain=fetch_on_chain,
+            agg=agg
+        )
+        return self._execute_request(endpoint)
+
+    def crvusd_market_supply(self, chain: str, start: int, end: int):
+        endpoint = f"crvusd/markets/{chain}/supply" + self._build_query(False, start=start, end=end)
+        return self._execute_request(endpoint)
+
+    def crvusd_loan_distribution(self, chain: str, controller_address: str):
+        endpoint = f"crvusd/markets/{chain}/{controller_address}/loans/distribution"
+        return self._execute_request(endpoint)
+
+    def crvusd_collateral_events(self, chain: str, controller_address: str, user: str, pagination: int=1000, page: int=1):
+        endpoint = f"crvusd/collateral_events/{chain}/{controller_address}/{user}" + self._build_query(
+            False,
+            pagination=pagination,
+            page=page
+        )
+        return self._execute_request(endpoint)
+
+    def pegkeepers(self, chain: str):
+        endpoint = f"crvusd/pegkeepers/{chain}"
+        return self._execute_request(endpoint)
+
+    def pegkeeper(self, chain: str, address: str, page: int=1, pagination: int=1000):
+        endpoint = f"crvusd/pegkeepers/{chain}/{address}" + self._build_query(
+            False,
+            page=page,
+            pagination=pagination
+        )
+        return self._execute_request(endpoint)
+
+    def llamma_events(self, chain: str, address: str, page: int=1, pagination: int=1000):
+        endpoint = f"crvusd/llamma_events/{chain}/{address}" + self._build_query(False, page=page, pagination=pagination)
+        return self._execute_request(endpoint)
+
+
+
 
 
